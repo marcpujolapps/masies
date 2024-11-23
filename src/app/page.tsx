592150d45c894ee1d208cf3a6ccd81865ae70cf6
Marc Pujol Gualdo <marcpujolapps@gmail.com>
2024-11-23 15:32:09 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import Main from "@/components/Main";
 import dynamic from "next/dynamic";
 
+const DynamicMain = dynamic(() => import("@/components/Main"), {
+  ssr: false,
+});
+
 export default function DynamicWrapper() {
-  return <Main />;
+  return <DynamicMain />;
 }
